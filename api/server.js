752721f5856b70d552ff5734b0ff7576637fc3f7
Matthew Knop <knopmatthew93@gmail.com>
2021-03-01 14:50:08 -0700

    Connection established wih api and mongo.

--- api/server.js
+++ api/server.js
@@ -1,38 +1,43 @@
-import express from 'express';
+import express, { response } from 'express';
 import dotenv from 'dotenv';
 import MongoClient from 'mongodb';
 
 const app = express();
-const users = [];
 
 dotenv.config({path: '../.env'});
 
-MongoClient.connect(process.env.MONGODB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
-    if(!err) {
-        console.log("Database Connection Established.");
-    } else {
-        console.log("There was an error connecting.");
-    }
-});
-
 app.use(express.json());
-
-app.get('/api/users', (req, res) => {
-  console.log('api/users called!')
-  res.json(users);
-});
-
-app.post('/api/user', (req, res) => {
-  const newUser = req.body.user;
-  console.log('Adding user: ', newUser);
-  users.push(newUser);
-  res.json("user added");
-});
-
-app.get('/', (req,res) => {
-    res.send('App Works!');
-});
 
 app.listen(process.env.API_PORT, () => {
     console.log(`API listening at http://localhost:${process.env.API_PORT}`);
+
+    MongoClient.connect(process.env.MONGODB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+        if(!err) console.log("Database connection established.");
+        else console.log("There was an error connecting.");
+    
+        const database = client.db("mydb");
+        const collection = database.collection("Users");
+        
+        app.post('/api/user', (req, res) => {
+            collection.insertOne((req.body), () => {
+                if (err) return res.status(500).send(err);
+
+                res.send(req.body);
+                const newUser = req.body;
+                console.log('Adding user: ', newUser);
+            })
+        });
+
+        app.get('/api/users', (req, res) => {
+            collection.find({}).toArray(function(result) {
+                if (err) throw err;
+                console.log(res.json(result));
+                console.log('All users retrieved from api/users');
+            })
+        });
+          
+        app.get('/', (req,res) => {
+            res.send('App Works!');
+        });
+    });
 });
